df49b6151a58faf7fc47403c4923999b45a8a5b9
sarah1reindl <sarah1.reindl@epitech.digital>
2024-12-05 16:57:00 +0100

    day1

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className= "flex item-center justify-between py-5 font-medium">
+
+    </div>
+  )
+}
+
+export default Navbar
